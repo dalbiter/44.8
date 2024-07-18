ed16964d11ff5745d9c0343d06356cce3977123e
danx <rushn24@gmail.com>
2024-07-17 22:17:19 -0400

    added UserForm

---
+++ src/UserForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="userform" />
+            <button>Add me to list!</button>
+        </form>
+    )
+};
+
+export default UserForm;
