11ef18703dee51a976ef23c5c0389972f818faa9
danx <rushn24@gmail.com>
2024-07-18 22:09:04 -0400

    multiple inputs

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,31 +1,52 @@
 import React, { useState } from 'react';
 
 const UserForm = () => {
-    const [username, setUsername] = useState("")
-    const [email, setEmail] = useState("")
+    const initialState = {
+        username: "",
+        email: "",
+        password: ""
+    }
+    const [formData, setFormData] = useState(initialState)
     const handleChange = (e) => {
-        setUsername(e.target.value)
+        const { name, value } = e.target;
+        setFormData(data => ({
+            ...data,
+            [name]: value
+        }))
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        alert(`Created user ${username}`)
-        setUsername("")
+        const { username, email, password } = formData
+        alert(`Created user ${username} with email ${email} and password ${password}`)
+        setFormData(initialState)
     }
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="username">Username</label>
             <input type="text"
                    id="username"
-                   placeholder="username" 
-                   value={username}    
-                   onChange={handleChange} />
+                   placeholder="username"
+                   name="username" 
+                   value={formData.username}    
+                   onChange={handleChange} 
+            />
             <label htmlFor="email">Email</label>
             <input type="email"
                    id="email"
-                   placeholder="email" 
-                   value={email}    
-                   onChange={handleChange} />
+                   placeholder="email"
+                   name="email" 
+                   value={formData.email}    
+                   onChange={handleChange} 
+            />
+            <label htmlFor="password">Password</label>
+            <input type="password"
+                   id="password"
+                   placeholder="password"
+                   name="password" 
+                   value={formData.password}    
+                   onChange={handleChange} 
+            />
             <button>Add me to list!</button>
         </form>
     )
