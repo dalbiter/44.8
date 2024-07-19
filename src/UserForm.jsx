bd2736bf3483b1e52235da1af9b9718367b71fb3
danx <rushn24@gmail.com>
2024-07-18 21:48:12 -0400

    onChange

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserForm = () => {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const handleChange = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Created user ${username}`)
+        setUsername("")
+    }
     return (
-        <form>
-            <input type="text" placeholder="userform" />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input type="text"
+                   id="username"
+                   placeholder="username" 
+                   value={username}    
+                   onChange={handleChange} />
+            <label htmlFor="email">Email</label>
+            <input type="email"
+                   id="email"
+                   placeholder="email" 
+                   value={email}    
+                   onChange={handleChange} />
             <button>Add me to list!</button>
         </form>
     )
